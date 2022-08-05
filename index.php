d9cf40ce010f62d88046810247baa7f03a40a412
20052022 <108892521+20052022@users.noreply.github.com>
2022-08-05 16:27:52 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drogueria Farmacon</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <header>
+        <img src="img/logo.png" alt="">
+        <h1>DROGUERIA FARMACON</h1>
+    </header>
+    
+    <nav>
+        <ul class="cont-ul">
+            <li class="develop">Salud y medicamentos
+                <ul class="ul-second">
+                    <li>Cuidado de la vista</li>
+                    <li>Salud digestiva</li>
+                </ul>
+            </li>
+            <li class="develop">Cuidado del bebe </li>
+            <li class="develop">Cuidado personal </li>
+            <li class="develop">Belleza </li>
+        </ul>
+    </nav>
+    
+    <main>
+        <h4>Contenido de la pagina</h4>
+    </main>
+    
+    <footer>
+        <h6>Pie de pagina</h6>
+    </footer>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <hgroup>
+    <h1>Bienvenido</h1>
+    <h3>Drogeria Farmacon</h3>
+    </hgroup>
+    <form>
+    <div class="group">
+        <input type="text"><span class="highlight"></span><span class="bar"></span>
+        <label>Usuario</label>
+    </div>
+    <div class="group">
+        <input type="email"><span class="highlight"></span><span class="bar"></span>
+        <label>Contraseña</label>
+    </div>
+    <a href="index.php"class="button buttonBlue">Ingresar</a>
+
+    </form>
+    <footer>
+        <img src="logo.png">
+    </footer>
+</body>
+</html>
